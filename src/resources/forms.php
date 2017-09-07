d9feebaa7a9a58d46ee8f54feb0c45fcaa6d2646
Artur <thetofiq@gmail.com>
2017-09-07 16:26:07 +0200

    Form update

--- src/resources/forms.php
+++ src/resources/forms.php
@@ -20,15 +20,20 @@ return [
         'submitLabel' => 'Zamów',
         'fields' => [
             'box' => ['title' => 'Opakowanie:', 'form' => 'select', 'required' => true],
-            'amount' => ['title' => 'Liczba opakowań:', 'form' => 'select', 'required' => true, 'value' => '1'],
+            'amount' => ['title' => 'Liczba opakowań:', 'form' => 'number', 'required' => true, 'value' => '1'],
             'payment' => ['title' => 'Sposób zapłaty:', 'form' => 'select', 'required' => true, 'value' => 'cash'],
             'delivery' => ['title' => 'Sposób dostawy:', 'form' => 'select', 'required' => true, 'value' => 'personal'],
-	          'deliveryAdress' => ['title' => 'Adres dostawy:', 'form' => 'textarea'],
+	          'deliveryName' => ['title' => 'Nazwa:', 'form' => 'text'],
+            'deliveryStreet' => ['title' => 'Ulica:', 'form' => 'text'],
+            'deliveryCode' => ['title' => 'Kod pocztowy:', 'form' => 'text'],
+            'deliveryCity' => ['title' => 'Miasto:', 'form' => 'text'],
             'notes' => ['title' => 'Uwagi dodatkowe:', 'form' => 'textarea'],
+            'invoice' => ['title' => 'Faktura:', 'form' => 'select', 'required' => true, 'value' => 'no'],
             'invName' => ['title' => 'Nazwa:', 'form' => 'text'],
 	          'nip' => ['title' => 'NIP:', 'form' => 'text'],
-	          'invAdress' => ['title' => 'Adres do faktury:', 'form' => 'textarea'],
-            'invoice' => ['title' => 'Faktura:', 'form' => 'select', 'required' => true, 'value' => 'no'],
+            'invStreet' => ['title' => 'Ulica:', 'form' => 'text'],
+            'invCode' => ['title' => 'Kod pocztowy:', 'form' => 'text'],
+            'invCity' => ['title' => 'Miasto:', 'form' => 'text'],
             'phone' => ['title' => 'Numer telefonu:', 'form' => 'number', 'required' => true, 'error' => 'Proszę wprowadzić numer telefonu'],
             'email' => ['title' => 'Email:', 'form' => 'email', 'required' => true],
         ],
@@ -38,7 +43,7 @@ return [
           'delivery' => ['personal' => 'Odbiór osobisty', 'mail' => 'Poczta', 'courier' => 'Kurier'],
           'amount' => ['1' => '1', '2' => '2', '3' => '3', '4' => '4', '5' => '5', '6' => '6', '7' => '7', '8' => '8', '9' => '9', '10' => '10'],
           'product' => 'SELECT id, label FROM ' . QBWPLISTS_TABLE . 'product',
-          'box' => 'SELECT box.price AS option_key, CONCAT(pack.label, \' (\', pack.weight,  \'g)\') AS option_value ' .
+          'box' => 'SELECT CONCAT(box.price, \':\', pack.weight) AS option_key, CONCAT(pack.label, \' (\', pack.weight,  \'g)\') AS option_value ' .
                     'FROM ' . QBWPLISTS_TABLE . 'box box ' .
                     'LEFT JOIN ' . QBWPLISTS_TABLE . 'product pro ON box.product_id = pro.id ' .
                     'LEFT JOIN ' . QBWPLISTS_TABLE . 'package pack ON box.package_id = pack.id WHERE pro.id=',
