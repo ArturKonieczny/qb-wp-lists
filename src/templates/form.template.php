88bcd80b9d354a29ea3560be1cb97812c0ef8235
Artur <thetofiq@gmail.com>
2017-10-12 17:02:41 +0200

    form template fix

--- src/templates/form.template.php
+++ src/templates/form.template.php
@@ -4,4 +4,28 @@
     window.app = window.app || {};
     window.app.data = <?php echo json_encode($this->datas) ?>;
     window.app.chosenProduct = window.location.search.substr(1);
+    window.app.deliveryOptions = [
+        {
+          id: 'personal',
+          label: 'Odbiór Osobisty',
+          tresholds: [-1]
+        },
+        {
+          id: 'post24',
+          label: 'Poczta, Paczka24',
+          tresholds: [20000, 10000, 5000, 2000, 500],
+          values: [27, 22, 18, 16, 12.5]
+        },
+        {
+          id: 'post48',
+          label: 'Poczta, Paczka48',
+          tresholds: [20000, 10000, 5000, 2000, 500],
+          values: [25.5, 20.5, 16.5, 14.5, 11]
+        }
+      ];
+      window.app.transferDetails = {
+        name: 'KEMI-PLAST Przedsiębiorstwo Wielobranżowe',
+        address: '61-655 Poznań ul. Gromadzka 17',
+        account: '50 1020 5558 1111 1205 4020 0038'
+      };
   </script>
